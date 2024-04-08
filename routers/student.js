440e3e510ba2b74bb25a8fd8874d700a9a74ba29
vnebra4niy <119805901+vnebra4niy@users.noreply.github.com>
2024-04-08 19:41:05 +0200

    3

--- routers/student.js
+++ routers/student.js
@@ -1,3 +1,6 @@
+let students=[]
+let nextId=1
+
 exports.getAddNewStudentPage = (req, res) => {
     res.render('Home', { title: 'Add new student' })
 }
@@ -7,5 +10,23 @@ exports.postAddNewStudent = (req, res) => {
     const code = req.body.code
     const fieldOfStudies = req.body.fieldOfStudies
 
+    const newStudent = {
+        id: nextId,
+        fullName: fullName,
+        code: code,
+        fieldOfStudies: fieldOfStudies
+    }
+
+    students.push(newStudent)
+    nextId++
+
     res.redirect('/success')
 }  
+
+exports.getAddingNewStudentSuccessPage = (req, res) => {
+    res.render('Success', { title: 'Success'})
+}
+  
+exports.getStudentsListPage = (req, res) => {
+    res.render('List', { title: 'List'})
+}
